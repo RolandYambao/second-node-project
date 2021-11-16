4040d79d992b5718269f86741130241059381d88
RolandYambao <rolandyambao@gmail.com>
2021-11-16 14:55:32 -0800

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const myModule = require("./utils/myModule.js");
+const fs = require("fs");
 
 console.log(myModule);
 console.log(myModule.newFunction());
@@ -6,3 +7,7 @@ console.log(myModule.newerFunction());
 console.log(myModule.newestFunction());
 console.log(myModule.newNumbers);
 console.log(myModule.newObject);
+
+fs.readFile('story.txt', 'utf8', function(err, data){
+        console.log(data);
+});
