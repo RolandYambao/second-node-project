dd1fc11e831e748123356f80b3b8f8598b777fb3
RolandYambao <rolandyambao@gmail.com>
2021-11-16 16:30:40 -0800

    Updated

--- index.js
+++ index.js
@@ -22,5 +22,26 @@ fs.readFile('story.txt', 'utf8', function (err, data) {
 // .listen(8000);
 
 const faker = require("faker");
+const { random } = require("faker");
 let randomName = faker.name.findName();
 console.log(randomName);
+
+const randomAddress = {
+    streetAddress: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zipCode: faker.address.zipCode(),
+}
+console.log(randomAddress);
+
+// import the module
+const axios = require('axios'); // allows to make API calls and retrieve data...
+
+// There is a function that exist on the frontend that allows us to retrieve data. What function? - fetch
+
+axios.get('https://api.github.com/users/romebell')
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
